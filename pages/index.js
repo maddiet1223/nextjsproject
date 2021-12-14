770a78557f3ad0f3243b5bfdbdc18e2fb5cc7245
Tugberk Goc <tugberk.goc.ext@bayer.com>
2021-12-14 18:11:13 -0500

    basic api implementation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
 import NavBar from '../components/NavBar'
 
-export default function Home () {
+export default function Home (props) {
+  console.log(props)
+
+  const test = async () => {
+    // update email, name, other informations PUT,POST
+    const data = await fetch('http://localhost:3000/api/users').then(res =>
+      res.json()
+    )
+
+    console.log(data)
+  }
+
   return (
     <div>
       <NavBar />
+      <div onClick={test}>Try me</div>
     </div>
   )
+}
+
+export async function getStaticProps () {
+  // before rendering user page GET
+  // get email, name, surname and other informations
+  const data = await fetch('http://localhost:3000/api/users').then(res =>
+    res.json()
+  )
+
+  return {
+    props: {
+      data
+    }
+  }
 }
